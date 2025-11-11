ea685f98f47578d1be15959bd2e0d7ff4a32a170
Highoncaffiene <ayushkumar5221@gmail.com>
2025-11-11 23:34:21 +0530

    Fixed the case sensitvity issue

--- src/Components/Common/Stairs.jsx
+++ src/Components/Common/Stairs.jsx
@@ -36,7 +36,7 @@ const Stairs = (props) => {
 
         gsap.from(pageRef.current,{
             opacity:0,
-            delay:1,
+            delay:1   ,
             scale:1.2
         })
     }, [currentPath])
